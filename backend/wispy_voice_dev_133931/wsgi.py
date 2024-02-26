18a45ae437b408aa1b999f4ee19e31283bf918ba
Crowdbotics <engineering@crowdbotics.com>
2024-02-26 17:05:36 +0000

    Initial commit

---
+++ backend/wispy_voice_dev_133931/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for wispy_voice_dev_133931 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "wispy_voice_dev_133931.settings"
+)
+
+application = get_wsgi_application()
